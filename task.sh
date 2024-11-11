bb0f03744cf91093a7aed2bd2887dd73d8d5b9ec
Gariman <garimanblack1@gmail.com>
2024-11-11 18:03:05 +0200

    Solution backup_DB fix words

--- task.sh
+++ task.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-USER="$DB_USER"
-PASSWORD="$DB_PASSWORD"
+USER=$DB_USER
+PASSWORD=$DB_PASSWORD
 
 
-mysqldump -u "${USER}" -p"${PASSWORD}" --database ShopDB --no-create-db --result-file=backup-db.sql
-mysql -u "${USER}" -p"${PASSWORD}" ShopDBReserve < backup-db.sql
+mysqldump -u "$USER" -p"$PASSWORD" --database ShopDB --no-create-db --result-file=backup-db.sql
+mysql -u "$USER" -p"$PASSWORD" ShopDBReserve < backup-db.sql
 
 
-mysqldump -u "${USER}" -p"${PASSWORD}" -- database ShopDB --no-create-info --result-file=bakup-no-create-info.sql
-mysql -u "${USER}" -p"${PASSWORD}" ShopDBDevelopment < bakup-no-create-info.sql
+mysqldump -u "$USER" -p"$PASSWORD" -- database ShopDB --no-create-db --no-create-info --result-file=backup-no-create.sql
+mysql -u "$USER" -p"$PASSWORD" ShopDBDevelopment < backup-no-create.sql
