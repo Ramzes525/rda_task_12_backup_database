f749eb7903706d517a127ad08526cef575d01d95
Gariman <garimanblack1@gmail.com>
2024-11-11 17:14:51 +0200

    Solution backup_DB fix

--- task.sh
+++ task.sh
@@ -4,8 +4,8 @@ USER="$DB_USER"
 PASSWORD="$DB_PASSWORD"
 
 
-mysqldump -u "${USER}" -p"${PASSWORD}" --database ShopDB --no-create-db --result-file=backup-no-create-db.sql
-mysql -u "${USER}" -p"${PASSWORD}" ShopDBReserve < backup.sql
+mysqldump -u "${USER}" -p"${PASSWORD}" --database ShopDB --no-create-db --result-file=backup-db.sql
+mysql -u "${USER}" -p"${PASSWORD}" ShopDBReserve < backup-db.sql
 
 
 mysqldump -u "${USER}" -p"${PASSWORD}" -- database ShopDB --no-create-info --result-file=bakup-no-create-info.sql
